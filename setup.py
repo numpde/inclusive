c5d5795e89ebead858878febd8931e3590391a43
r <21158052+numpde@users.noreply.github.com>
2019-11-05 00:42:03 +0100

    RA: Version increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r') as fh:
 
 setuptools.setup(
 	name="inclusive",
-	version="0.0.1",
+	version="0.0.2",
 	author="RA",
 	author_email="numpde@null.net",
 	keywords="development range slice",
